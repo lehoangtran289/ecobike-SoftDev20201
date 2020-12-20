77acc9958523f17d1ca977c73406b0b9f980b4d6
Tran Le Hoang <lehoangtran289@gmail.com>
2020-12-20 12:17:12 +0700

    fix Bike: - id @nonnull - remove init Status - match() method

--- Homework05/Projects/EBRServer/src/main/java/com/hust/ebr/model/Bike.java
+++ Homework05/Projects/EBRServer/src/main/java/com/hust/ebr/model/Bike.java
@@ -3,20 +3,22 @@ package com.hust.ebr.model;
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonSubTypes.Type;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
-import com.fasterxml.jackson.annotation.JsonTypeName;
-import lombok.Data;
-import org.springframework.lang.NonNull;
+import lombok.*;
 import org.springframework.util.StringUtils;
 
 import java.util.Date;
 
-@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "type")
-@JsonTypeName("bike")
 @JsonSubTypes({@Type(value = NormalBike.class, name = "normalBike"),
         @Type(value = TwinBike.class, name = "twinBike"),
         @Type(value = EBike.class, name = "eBike")})
 public class Bike {
+    @NonNull
     private String id;
     private String name;
     private float weight;
@@ -24,10 +26,8 @@ public class Bike {
     private Date manufacturingDate;
     private String producer;
     private float cost;
-    private Status status = Status.Available;
+    private Status status;
     private String dockingStationId;
-
-    public enum Status {Available, Renting};
 
     public boolean match(Bike bike) {
         if (bike == null)
@@ -42,13 +42,13 @@ public class Bike {
         if (bike.weight != 0 && this.weight != bike.weight) {
             return false;
         }
-        if (bike.licensePlate != null && !bike.licensePlate.equals("") && !this.name.contains(bike.licensePlate)) {
+        if (bike.licensePlate != null && !bike.licensePlate.equals("") && !this.licensePlate.contains(bike.licensePlate)) {
             return false;
         }
         if (bike.manufacturingDate != null && this.manufacturingDate.equals(bike.manufacturingDate)) {
             return false;
         }
-        if (bike.producer != null && !bike.producer.equals("") && !this.name.contains(bike.producer)) {
+        if (bike.producer != null && !bike.producer.equals("") && !this.producer.contains(bike.producer)) {
             return false;
         }
         if (bike.cost != 0 && this.cost != bike.cost) {
@@ -70,5 +70,7 @@ public class Bike {
         }
         return false;
     }
+
+    public enum Status {Available, Renting}
 
 }
