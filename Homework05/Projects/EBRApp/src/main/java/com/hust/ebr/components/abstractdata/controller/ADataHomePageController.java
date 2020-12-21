f612b19db46948e91f6d18928d66ace2de410c28
tran-haison <haison250199@gmail.com>
2020-12-21 10:42:40 +0700

    Add new packages

---
+++ Homework05/Projects/EBRApp/src/main/java/com/hust/ebr/components/abstractdata/controller/ADataHomePageController.java
@@ -0,0 +1,40 @@
+package com.hust.ebr.components.abstractdata.controller;
+
+import com.hust.ebr.components.abstractdata.gui.ADataHomePagePane;
+import com.hust.ebr.components.abstractdata.gui.ADataListPane;
+import com.hust.ebr.components.abstractdata.gui.ADataSearchPane;
+import com.hust.ebr.components.abstractdata.gui.ADataSinglePane;
+
+import javax.swing.*;
+import java.util.List;
+import java.util.Map;
+
+public abstract class ADataHomePageController<T> {
+    private final ADataHomePagePane<T> homePagePane;
+
+    public ADataHomePageController() {
+        ADataSearchPane searchPane = createSearchPane();
+        ADataListPane<T> listPane = createListPane();
+
+        searchPane.setSearchController(searchParams -> {
+            List<? extends T> list = ADataHomePageController.this.search(searchParams);
+            listPane.updateData(list);
+        });
+
+        searchPane.fireSearchEvent();
+
+        homePagePane = new ADataHomePagePane<T>(searchPane, listPane);
+    }
+
+    public JPanel getHomePagePane() {
+        return homePagePane;
+    }
+
+    public abstract ADataSearchPane createSearchPane();
+
+    public abstract List<? extends T> search(Map<String, String> searchParams);
+
+    public abstract ADataSinglePane<T> createSinglePane();
+
+    public abstract ADataListPane<T> createListPane();
+}
