4895de50774dfe55987eea1388296690200bcf77
tran-haison <haison250199@gmail.com>
2020-12-21 10:39:29 +0700

    Init controller for EBRAdmin

--- Homework05/Projects/EBRApp/src/main/java/com/hust/ebr/app/admin/EBRAdminController.java
+++ Homework05/Projects/EBRApp/src/main/java/com/hust/ebr/app/admin/EBRAdminController.java
@@ -1,4 +1,19 @@
 package com.hust.ebr.app.admin;
 
+import com.hust.ebr.beans.DockingStation;
+import com.hust.ebr.components.abstractdata.controller.ADataHomePageController;
+import com.hust.ebr.components.dockingstation.controller.AdminHomePageController;
+
+import javax.swing.*;
+
 public class EBRAdminController {
+
+    public EBRAdminController() {
+
+    }
+
+    public JPanel getHomePage() {
+        ADataHomePageController<DockingStation> controller = new AdminHomePageController();
+        return controller.getHomePagePane();
+    }
 }
