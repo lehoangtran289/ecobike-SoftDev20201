25001abec3d44fc9f4d84474ee5668ebfd23570f
tran-haison <haison250199@gmail.com>
2020-12-21 12:27:42 +0700

    Init constructor

--- Homework05/Projects/EBRApp/src/main/java/com/hust/ebr/components/dockingstation/controller/AdminHomePageController.java
+++ Homework05/Projects/EBRApp/src/main/java/com/hust/ebr/components/dockingstation/controller/AdminHomePageController.java
@@ -5,9 +5,10 @@ import com.hust.ebr.components.abstractdata.controller.ADataHomePageController;
 import com.hust.ebr.components.abstractdata.gui.ADataListPane;
 import com.hust.ebr.components.abstractdata.gui.ADataSearchPane;
 import com.hust.ebr.components.abstractdata.gui.ADataSinglePane;
-
-import java.util.List;
-import java.util.Map;
+import com.hust.ebr.components.dockingstation.gui.AdminDockingStationListPane;
+import com.hust.ebr.components.dockingstation.gui.DockingStationSearchPane;
+import com.hust.ebr.components.dockingstation.gui.DockingStationSinglePane;
+import com.hust.ebr.serverapi.DockingStationApi;
 
 public class AdminHomePageController extends ADataHomePageController<DockingStation> {
 
@@ -17,21 +18,25 @@ public class AdminHomePageController extends ADataHomePageController<DockingStat
 
     @Override
     public ADataSearchPane createSearchPane() {
-        return null;
+        return new DockingStationSearchPane();
     }
 
     @Override
     public ADataSinglePane createSinglePane() {
-        return null;
+        return new DockingStationSinglePane();
     }
 
     @Override
     public ADataListPane createListPane() {
-        return null;
+        return new AdminDockingStationListPane(this);
     }
 
     @Override
-    public List search(Map searchParams) {
-        return null;
+    public DockingStation search(String searchParams) {
+        return new DockingStationApi().getStationById(searchParams);
+    }
+
+    public DockingStation updateDockingStation(DockingStation dockingStation) {
+        return new DockingStationApi().updateStation(dockingStation);
     }
 }
