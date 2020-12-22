b66fc46ebb3892763ee0ec0bee9b729167dc14c8
tran-haison <haison250199@gmail.com>
2020-12-22 11:24:38 +0700

    Add Bike tab and History tab to Admin homepage

--- Homework05/Projects/EBRApp/src/main/java/com/hust/ebr/app/admin/EBRAdminController.java
+++ Homework05/Projects/EBRApp/src/main/java/com/hust/ebr/app/admin/EBRAdminController.java
@@ -1,8 +1,11 @@
 package com.hust.ebr.app.admin;
 
+import com.hust.ebr.beans.Bike;
 import com.hust.ebr.beans.DockingStation;
 import com.hust.ebr.components.abstractdata.controller.ADataHomePageController;
-import com.hust.ebr.components.dockingstation.controller.AdminHomePageController;
+import com.hust.ebr.components.bike.controller.AdminBikePageController;
+import com.hust.ebr.components.dockingstation.controller.AdminStationPageController;
+import com.hust.ebr.components.history.controller.AdminHistoryPageController;
 
 import javax.swing.*;
 
@@ -12,8 +15,18 @@ public class EBRAdminController {
 
     }
 
-    public JPanel getHomePage() {
-        ADataHomePageController<DockingStation> controller = new AdminHomePageController();
+    public JPanel getStationPage() {
+        ADataHomePageController<DockingStation> controller = new AdminStationPageController();
+        return controller.getHomePagePane();
+    }
+
+    public JPanel getBikePage() {
+        ADataHomePageController<Bike> controller = new AdminBikePageController();
+        return controller.getHomePagePane();
+    }
+
+    public JPanel getHistoryPage() {
+        ADataHomePageController<> controller = new AdminHistoryPageController();
         return controller.getHomePagePane();
     }
 }
