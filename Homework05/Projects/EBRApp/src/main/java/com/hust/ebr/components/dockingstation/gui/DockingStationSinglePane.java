402b20b27dd46d72e321f43cb7e9b1a8d9b9e5b9
tran-haison <haison250199@gmail.com>
2020-12-22 11:28:50 +0700

    Modify single pane of docking station

--- Homework05/Projects/EBRApp/src/main/java/com/hust/ebr/components/dockingstation/gui/DockingStationSinglePane.java
+++ Homework05/Projects/EBRApp/src/main/java/com/hust/ebr/components/dockingstation/gui/DockingStationSinglePane.java
@@ -5,6 +5,16 @@ import com.hust.ebr.components.abstractdata.gui.ADataSinglePane;
 
 public class DockingStationSinglePane extends ADataSinglePane<DockingStation> {
 
+    public DockingStationSinglePane() {
+        super();
+    }
+
+    @Override
+    public void buildControls() {
+        super.buildControls();
+
+    }
+
     @Override
     public void displayData() {
 
