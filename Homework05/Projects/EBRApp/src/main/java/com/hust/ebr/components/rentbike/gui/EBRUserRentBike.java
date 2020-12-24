d7aa473c805ba10ec87a66d603caf64c5e9f636c
tran-haison <haison250199@gmail.com>
2020-12-24 11:16:18 +0700

    Delete unused class to re-design new one

--- Homework05/Projects/EBRApp/src/main/java/com/hust/ebr/components/rentbike/gui/EBRUserRentBike.java
+++ Homework05/Projects/EBRApp/src/main/java/com/hust/ebr/components/rentbike/gui/EBRUserRentBike.java
@@ -5,27 +5,76 @@ import com.hust.ebr.beans.CreditCard;
 import com.hust.ebr.components.rentbike.controller.EBRUserRentBikeController;
 
 import javax.swing.*;
-import java.awt.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import static com.hust.ebr.utils.Constants.WINDOW_HEIGHT;
 import static com.hust.ebr.utils.Constants.WINDOW_WIDTH;
 
-public class EBRUserRentBike extends JFrame {
+public class EBRUserRentBike extends JFrame{
+
+    private JPanel rootPanel;
+    private JLabel labelCardOwner;
+    private JLabel labelCardNumber;
+    private JLabel labelCurrentDate;
+    private JLabel labelId;
+    private JLabel labelName;
+    private JLabel labelWeight;
+    private JLabel labelLicensePlate;
+    private JLabel labelManufactureDate;
+    private JLabel labelProducer;
+    private JLabel labelCost;
+    private JLabel labelStatus;
+    private JLabel labelFromDocking;
+    private JButton buttonReturnBike;
+
+    private CreditCard creditCard;
+    private Bike bike;
+    private long timeBegin;
 
     public EBRUserRentBike(EBRUserRentBikeController controller, Bike bike, CreditCard creditCard) {
-        JPanel rootPanel = new JPanel();
-        setContentPane(rootPanel);
+        this.bike = bike;
+        this.creditCard = creditCard;
+        timeBegin = System.currentTimeMillis();
 
-        BorderLayout layout = new BorderLayout();
-        rootPanel.setLayout(layout);
-
-        JPanel rentBikePage = controller.getRentBikePage(bike, creditCard);
-        rootPanel.add(rentBikePage);
-
-        setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
-        setLocationRelativeTo(null);
         setTitle("Rent Bike Detail");
+        setContentPane(rootPanel);
         setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
         setVisible(true);
+
+        displayData();
+        handleButtonEvent();
+    }
+
+    public void displayData() {
+        if (bike != null) {
+            labelId.setText("ID: " + bike.getId());
+            labelName.setText("Name: " + bike.getName());
+            labelWeight.setText("Weight: " + bike.getWeight());
+            labelLicensePlate.setText("License plate: " + bike.getLicensePlate());
+            labelManufactureDate.setText("Manufacture date: " + bike.getManufacturingDate());
+            labelProducer.setText("Producer: " + bike.getProducer());
+            labelCost.setText("Cost: " + bike.getCost());
+            labelStatus.setText("Status: " + bike.getStatus());
+            labelFromDocking.setText("From docking: " + bike.getDockingStationId());
+        }
+
+        if (creditCard != null) {
+            labelCardOwner.setText("Card owner: " + creditCard.getCardOwner());
+            labelCardNumber.setText("Card number: " + creditCard.getCardNumber());
+        }
+
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        Date currentDate = new Date();
+        labelCurrentDate.setText(simpleDateFormat.format(currentDate));
+    }
+
+    private void handleButtonEvent() {
+        buttonReturnBike.addActionListener(e -> {
+            // TODO
+
+        });
     }
 }
