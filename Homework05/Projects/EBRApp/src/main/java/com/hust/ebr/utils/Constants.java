31066c89bf105830bd5937fb5c74c2cfe978cd16
tran-haison <haison250199@gmail.com>
2020-12-24 10:45:01 +0700

    Call EBRRentBikePage instance

--- Homework05/Projects/EBRApp/src/main/java/com/hust/ebr/utils/Constants.java
+++ Homework05/Projects/EBRApp/src/main/java/com/hust/ebr/utils/Constants.java
@@ -4,4 +4,8 @@ public class Constants {
 
     public static final int WINDOW_WIDTH = 800;
     public static final int WINDOW_HEIGHT = 550;
+
+    public static final double NORMAL_BIKE_DEPOSIT = 400000;
+    public static final double EBIKE_DEPOSIT = 700000;
+    public static final double TWIN_BIKE_DEPOSIT = 550000;
 }
