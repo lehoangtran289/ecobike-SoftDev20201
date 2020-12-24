ee0acc367baaf50179d1fcc27679631d5cd39261
Tran Le Hoang <lehoangtran289@gmail.com>
2020-12-24 23:32:13 +0700

    - remove irrelevant fields of Station edit

--- Homework05/Projects/EBRApp/src/main/java/com/hust/ebr/components/dockingstation/gui/DockingStationEditDialog.java
+++ Homework05/Projects/EBRApp/src/main/java/com/hust/ebr/components/dockingstation/gui/DockingStationEditDialog.java
@@ -10,10 +10,6 @@ public class DockingStationEditDialog extends ADataEditDialog<DockingStation> {
 
     private JTextField nameField;
     private JTextField addressField;
-    private JTextField emptyDockField;
-    private JTextField normalBikeField;
-    private JTextField twinBikeField;
-    private JTextField eBikeField;
 
     public DockingStationEditDialog(DockingStation dockingStation, IDataManageController<DockingStation> controller) {
         super(dockingStation, controller);
@@ -38,34 +34,14 @@ public class DockingStationEditDialog extends ADataEditDialog<DockingStation> {
         addressField = new JTextField(15);
         addressField.setText(t.getStationAddress());
 
-        emptyDockField = new JTextField(15);
-        emptyDockField.setText(String.valueOf(t.getEmptyDockCount()));
-
-        normalBikeField = new JTextField(15);
-        normalBikeField.setText(String.valueOf(t.getNormalBikeCount()));
-
-        twinBikeField = new JTextField(15);
-        twinBikeField.setText(String.valueOf(t.getTwinBikeCount()));
-
-        eBikeField = new JTextField(15);
-        eBikeField.setText(String.valueOf(t.getEBikeCount()));
-
         addNewField(nameField, "Station name");
         addNewField(addressField, "Station address");
-        addNewField(emptyDockField, "Empty docks");
-        addNewField(normalBikeField, "Normal bikes");
-        addNewField(twinBikeField, "Twin bikes");
-        addNewField(eBikeField, "EBikes");
     }
 
     @Override
     public DockingStation getNewData() {
         t.setStationName(nameField.getText());
         t.setStationAddress(addressField.getText());
-        t.setEmptyDockCount(Integer.parseInt(emptyDockField.getText()));
-        t.setNormalBikeCount(Integer.parseInt(normalBikeField.getText()));
-        t.setTwinBikeCount(Integer.parseInt(twinBikeField.getText()));
-        t.setEBikeCount(Integer.parseInt(eBikeField.getText()));
 
         return t;
     }
