b62754f0701442ff4e454ce6fa74540962bdcd73
Tran Le Hoang <lehoangtran289@gmail.com>
2020-12-27 12:35:42 +0700

    - rename home->data

--- Homework05/Projects/EBRApp/src/main/java/com/hust/ebr/components/bike/gui/AdminBikeListPane.java
+++ Homework05/Projects/EBRApp/src/main/java/com/hust/ebr/components/bike/gui/AdminBikeListPane.java
@@ -2,7 +2,6 @@ package com.hust.ebr.components.bike.gui;
 
 import com.hust.ebr.beans.Bike;
 import com.hust.ebr.components.abstractdata.controller.ADataPageController;
-import com.hust.ebr.components.abstractdata.controller.IDataUpdateController;
 import com.hust.ebr.components.abstractdata.gui.ADataListPane;
 import com.hust.ebr.components.abstractdata.gui.ADataSinglePane;
 import com.hust.ebr.components.bike.controller.AdminBikePageController;
@@ -12,7 +11,7 @@ import javax.swing.*;
 public class AdminBikeListPane extends ADataListPane<Bike> {
 
     public AdminBikeListPane(ADataPageController<Bike> controller) {
-        this.homePageController = controller;
+        this.dataPageController = controller;
     }
 
     @Override
@@ -20,6 +19,6 @@ public class AdminBikeListPane extends ADataListPane<Bike> {
         JButton button = new JButton("Edit");
         singlePane.addDataHandlingComponent(button);
 
-        ((AdminBikePageController) homePageController).onEdit(singlePane, button);
+        ((AdminBikePageController) dataPageController).onEdit(singlePane, button);
     }
 }
--- Homework05/Projects/EBRApp/src/main/java/com/hust/ebr/components/dockingstation/gui/AdminStationListPane.java
+++ Homework05/Projects/EBRApp/src/main/java/com/hust/ebr/components/dockingstation/gui/AdminStationListPane.java
@@ -2,8 +2,6 @@ package com.hust.ebr.components.dockingstation.gui;
 
 import com.hust.ebr.beans.DockingStation;
 import com.hust.ebr.components.abstractdata.controller.ADataPageController;
-import com.hust.ebr.components.abstractdata.controller.IDataManageController;
-import com.hust.ebr.components.abstractdata.controller.IDataUpdateController;
 import com.hust.ebr.components.abstractdata.gui.ADataListPane;
 import com.hust.ebr.components.abstractdata.gui.ADataSinglePane;
 import com.hust.ebr.components.dockingstation.controller.AdminStationPageController;
@@ -16,8 +14,8 @@ import java.awt.event.ActionListener;
 
 public class AdminStationListPane extends ADataListPane<DockingStation> {
 
-    public AdminStationListPane(ADataPageController<DockingStation> homePageController) {
-        this.homePageController = homePageController;
+    public AdminStationListPane(ADataPageController<DockingStation> dataPageController) {
+        this.dataPageController = dataPageController;
     }
 
     @Override
@@ -25,7 +23,7 @@ public class AdminStationListPane extends ADataListPane<DockingStation> {
         JButton button = new JButton("Edit");
         singlePane.addDataHandlingComponent(button);
 
-        ((AdminStationPageController) homePageController).onEdit(singlePane, button);
+        ((AdminStationPageController) dataPageController).onEdit(singlePane, button);
 
         JButton viewDockButton = new JButton("View Details");
         singlePane.addDataHandlingComponent(viewDockButton);
